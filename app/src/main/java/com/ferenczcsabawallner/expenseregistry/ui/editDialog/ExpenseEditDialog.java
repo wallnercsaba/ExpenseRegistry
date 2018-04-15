490a5055cb12816eed0f2fdfc30d4a2a17979925
wallnercsaba <wallnercsaba@gmail.com>
2018-04-15 20:43:50 +0200

    ExpenseEditDialog in progress

---
+++ app/src/main/java/com/ferenczcsabawallner/expenseregistry/ui/editDialog/ExpenseEditDialog.java
@@ -0,0 +1,13 @@
+package com.ferenczcsabawallner.expenseregistry.ui.editDialog;
+
+/**
+ * Created by Csabi on 2018. 04. 15..
+ */
+
+public class ExpenseEditDialog {
+    public ExpenseEditDialog(){
+        //Expense model
+    }
+
+
+}
