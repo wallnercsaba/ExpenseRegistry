8788043a55caf38354dd431b1303f26cb3388bc7
wallnercsaba <wallnercsaba@gmail.com>
2018-04-15 20:52:30 +0200

    context added to UIModule

--- app/src/main/java/com/ferenczcsabawallner/expenseregistry/ui/UIModule.java
+++ app/src/main/java/com/ferenczcsabawallner/expenseregistry/ui/UIModule.java
@@ -1,6 +1,9 @@
 package com.ferenczcsabawallner.expenseregistry.ui;
 
+import android.content.Context;
+
 import dagger.Module;
+import dagger.Provides;
 
 /**
  * Created by Csabi on 2018. 04. 15..
@@ -8,5 +11,14 @@ import dagger.Module;
 
 @Module
 public class UIModule {
+    private Context context;
 
+    public UIModule(Context context) {
+        this.context = context;
+    }
+
+    @Provides
+    public Context provideContext() {
+        return context;
+    }
 }
